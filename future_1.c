d47e57109190779152b8a22be8b70fc0e45471d0
kohee <koheetg@gmail.com>
2026-01-13 16:27:13 +0600

    Update future_1.c

--- future_1.c
+++ future_1.c
@@ -41,3 +41,41 @@ int main() {
 
     return 0;
 }
+///another future example of custom fucntions of open() 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int open_my_file(const char *file_name, int flags, mode_t perm) {
+    int fd = open(file_name, flags, perm);
+    if (fd < 0) {
+        perror("open failed");
+        exit(EXIT_FAILURE);
+    }
+    return fd;
+}
+
+void my_print(const char *message, ...) {
+    va_list ap;
+    va_start(ap, message);
+    vdprintf(STDOUT_FILENO, message, ap);
+    va_end(ap);
+}
+
+int main(void) {
+    const char *target_file = "log.c";
+    int fd = open_my_file(target_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    int n = dprintf(fd, "Writing something via dprintf and low level system call!\n");
+    if (n < 0) {
+        perror("Failed to write in the file");
+        close(fd);
+        return -1;
+    }
+    close(fd);
+    printf("DONE!\n");
+    my_print("Printing message via dprintf()\n");
+    return 0;
+}
